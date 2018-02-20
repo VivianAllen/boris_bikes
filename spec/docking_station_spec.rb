d5490b11fc893cc3b9287c7c50c96b7cdb7ca743
Vivian Allen <mrvivianallen@gmail.com>
2018-02-20 15:43:33 +0000

    exercise 11 done

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,22 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+
   it 'responds to release_bike method.' do
     docking_station = DockingStation.new
     expect(docking_station).to respond_to(:release_bike)
   end
+
+  it 'can dock a bike' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:dock_bike).with(1).argument
+  end
+
+  it 'docks a bike' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    docking_station.dock_bike(bike)
+    expect(docking_station.list_bikes[0]).to equal(bike)
+  end
+
 end
