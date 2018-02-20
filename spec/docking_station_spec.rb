c49045a44379916a13ba5f2bbf1b1bd1c9de1e64
Vivian Allen <mrvivianallen@gmail.com>
2018-02-20 17:12:41 +0000

    one-liner it test for docking station release function

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,8 @@ require 'docking_station.rb'
 
 describe DockingStation do
 
-  it 'responds to release_bike method.' do
-    docking_station = DockingStation.new
-    expect(docking_station).to respond_to(:release_bike)
+  describe 'responds to release_bike method.' do
+    it { should respond_to(:release_bike) }
   end
 
   it 'releases a bike that is working.' do
