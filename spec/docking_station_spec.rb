9813f10409ddb2315c746f72dcd4a3773a75db9e
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-02-20 15:45:43 +0000

    problem?

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,15 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+
   it 'responds to release_bike method.' do
     docking_station = DockingStation.new
     expect(docking_station).to respond_to(:release_bike)
   end
+
+  it 'releases a bike that is working.' do
+    bike = DockingStation.new.release_bike
+    expect(bike).to respond_to(:working?)
+  end
+
 end
