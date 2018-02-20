199cf6b9d5e389314721e4c9aa802dfe7f4563d9
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-02-20 14:48:15 +0000

    unit testing

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,7 @@
 class DockingStation
 
+  def release_bike
+    "here's a bike"
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-
+  it 'responds to release_bike method.' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
